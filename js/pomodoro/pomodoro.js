eeb0f8964cc0cb7ddcb10afb54c71641f769845f
Schmalodev <132712267+Schmalodev@users.noreply.github.com>
2025-06-28 13:12:27 +0200

    feat reseater and test featch

--- js/pomodoro/pomodoro.js
+++ js/pomodoro/pomodoro.js
@@ -1,10 +1,18 @@
 const button = document.getElementById("start_btn");
+const reset = document.getElementById("reset");
+const save = document.getElementById("save");
+
 var count = document.getElementById("timer_count");
+var name = document.getElementById("name").value;
 var sek = 0;
+var min = 0;
+var hour = 0;
+
+let stoppuhr = null
 
 button.addEventListener("click", function(){
     if(button.value == "Start"){
-        const stoppuhr = setInterval(()=>{
+        stoppuhr = setInterval(()=>{
             button.value = "Stop"
             sek++;
             const minut = Math.floor(sek / 60);
@@ -14,6 +22,28 @@ button.addEventListener("click", function(){
             count.innerHTML = hour+":"+minut+":"+restSek
         }, 100)
     }else{
-        window.location.href = "../html/pomodoro.html"
+        button.value = "Start";
+        clearInterval(stoppuhr);
     }
+})
+
+reset.addEventListener("click", function(){
+    window.location.href = "../html/pomodoro.html"
+})
+
+save.addEventListener("click", function(){
+    fetch("https://workback-16op.onrender.com/",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body:{
+            "Sek": sek,
+            "Min": min,
+            "Houre": hour,
+            "Names": "name"
+        }
+    })
+    .then(data => data.json())
+    .then(response => console.log(response))
 })
